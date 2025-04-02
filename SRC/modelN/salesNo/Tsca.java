c653e4c248d6d4a60ff6a152a455a292bcdffe20
marswang <mars.wang@ts.com.tw>
2025-04-02 11:56:07 +0800

    (fix): 修正shipMethod對應不到lookup_code

--- SRC/modelN/salesNo/Tsca.java
+++ SRC/modelN/salesNo/Tsca.java
@@ -440,7 +440,7 @@ public class Tsca extends ModelNCommonUtils {
             ResultSet rs = this.getFndLookupValuesData();
             if (!rs.isBeforeFirst()) rs.beforeFirst();
             while (rs.next()) {
-                if (rs.getString("MEANING").equals(modelNDto.getTransportation())) {
+                if (rs.getString("MEANING").equals(modelNDto.getShippingMethod())) {
                     modelNDto.setShippingMethod(rs.getString("LOOKUP_CODE"));
                     break;
                 }
